b13e7cdbcdf6bbf85057cc56eb7b64ac4b686ec6
matthewgodin <matthew@skillbookacademy.com>
2021-07-03 18:59:53 -0400

    Start over with server with Oak

--- src/controllers/indexcontroller.ts
+++
@@ -1,6 +0,0 @@
-import {ServerRequest} from 'https://deno.land/std@0.97.0/http/server.ts';
-import {Status} from 'https://deno.land/std@0.97.0/http/http_status.ts';
-import {jsonResponse} from '../lib/quickResponses.ts';
-
-export const home: (req: ServerRequest) => Promise<void> = 
-    (req) => jsonResponse(req, Status.OK, {});
