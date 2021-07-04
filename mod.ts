a00e5f37303765ed803f0bc15c4a6faed9fbc22c
matthewgodin <matthew@skillbookacademy.com>
2021-07-04 00:19:08 -0400

    FINALLY WORKINGgit add .!

--- mod.ts
+++ mod.ts
@@ -1,14 +1,24 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+import { Application, Router, send } from 'https://deno.land/x/oak/mod.ts';
 const env = Deno.env.toObject();
 const app = new Application();
 const router = new Router();
 
-router.get('/', (ctx) => {
-    ctx.response.body = 'Hello World';
-});
+/*router.get('/', (ctx) => {
+    ctx.response.body = 'Hello World'
+});*/
 
 const port: number = +env.LICODE_PORT || 3000;
-app.use(router.routes());
+app.addEventListener("error", (evt) => {
+    // Will log the thrown error to the console.
+    console.log(evt.error);
+  });
+app.use(async (context) => {
+    await send(context, context.request.url.pathname, {
+        root: `${Deno.cwd()}/react-app/build`,
+        index: "index.html",
+    });
+});
+//app.use(router.routes());
 app.use(router.allowedMethods());
 console.log('Running on port', port);
 await app.listen({ port });
