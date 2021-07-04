063a9e5d2c3bd4668fbaffeea84db6220e4e40a2
matthewgodin <matthew@skillbookacademy.com>
2021-07-03 21:30:44 -0400

    Initial hello world server, gitignore

--- mod.ts
+++ mod.ts
@@ -1,2 +1,14 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+const env = Deno.env.toObject();
 const app = new Application();
+const router = new Router();
+
+router.get('/', (ctx) => {
+    ctx.response.body = 'Hello World';
+});
+
+const port: number = +env.LICODE_PORT || 3000;
+app.use(router.routes());
+app.use(router.allowedMethods());
+console.log('Running on port', port);
+await app.listen({ port });
