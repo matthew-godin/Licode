b762c9f0df18781727f5e53641918958a6e7992b
matthewgodin <matthew@skillbookacademy.com>
2021-07-04 02:11:56 -0400

    first react component

---
+++ react-app/src/components/login.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export interface LoginProps {
+    
+}
+ 
+export interface LoginState {
+    
+}
+ 
+class Login extends React.Component<LoginProps, LoginState> {
+    //state = { :  }
+    render() { 
+        //return (  );
+        return <h1>Hello World</h1>;
+    }
+}
+ 
+export default Login;
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Login from './components/login';
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
