51a5a48cf86e50c44f0b945516683db37240daae
matthewgodin <matthew@skillbookacademy.com>
2021-07-05 03:43:55 -0400

    simple sign up form

--- react-app/src/components/signupForm.tsx
+++ react-app/src/components/signupForm.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Component } from "react";
 
 export interface SignUpFormProps {}
 
@@ -7,8 +6,38 @@ export interface SignUpFormState {}
 
 class SignUpForm extends React.Component<SignUpFormProps, SignUpFormState> {
   //state = { :  }
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Call the server
+    // Save the changes
+    // Redirect the user to a different page
+    console.log("Submitted");
+  };
   render() {
-    return <h1>Register</h1>;
+    return (
+      <div>
+        <h1>Sign up</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input id="email" type="text" placeholder="Email Address" />
+          </div>
+          <div>
+            <input id="username" type="text" placeholder="Username" />
+          </div>
+          <div>
+            <input id="password" type="text" placeholder="Password" />
+          </div>
+          <div>
+            <input
+              id="confirm-passord"
+              type="text"
+              placeholder="Confirm Password"
+            />
+          </div>
+          <button>Sign up</button>
+        </form>
+      </div>
+    );
   }
 }
 
