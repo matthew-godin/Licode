520d620544973eac9731964aa810eccd9a54cd55
matthewgodin <matthew@skillbookacademy.com>
2021-07-19 07:12:29 -0400

    user registration in progress

--- mod.ts
+++ mod.ts
@@ -1,7 +1,20 @@
-import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+import {
+    Application,
+    Router,
+    RouterContext,
+    Status,
+    send,
+} from "https://deno.land/x/oak/mod.ts";
 const env = Deno.env.toObject();
 const app = new Application();
 const router = new Router();
+
+interface User {
+    id: number;
+    email: string;
+    username: string;
+    password: string;
+}
 
 const port: number = +env.LICODE_PORT || 3000;
 app.addEventListener("error", (evt) => {
@@ -9,6 +22,33 @@ app.addEventListener("error", (evt) => {
 });
 router.get("/api/hello-world", (context) => {
     context.response.body = "Hello World";
+});
+router.post("/api/users", async (context: RouterContext) => {
+    if (!context.request.hasBody) {
+        context.throw(Status.BadRequest, "Bad Request");
+    }
+    const body = context.request.body();
+    let user: Partial<User> | undefined;
+    if (body.type === "json") {
+        user = await body.value;
+    }
+    if (user) {
+        context.assert(
+            user.email &&
+                typeof user.email === "string" &&
+                user.username &&
+                typeof user.username === "string" &&
+                user.password &&
+                typeof user.password === "string",
+            Status.BadRequest
+        );
+        // Save the book in the DB
+        context.response.status = Status.OK;
+        context.response.body = user;
+        context.response.type = "json";
+        return;
+    }
+    context.throw(Status.BadRequest, "Bad Request");
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
