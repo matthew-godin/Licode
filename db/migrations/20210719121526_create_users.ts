74baaae6e59e973542acf21f2cd4adc4dc856c15
matthewgodin <matthew@skillbookacademy.com>
2021-07-25 13:27:45 -0400

    first user db migration

--- db/migrations/20210719121526_create_users.ts
+++ db/migrations/20210719121526_create_users.ts
@@ -9,8 +9,9 @@ export default class extends AbstractMigration<ClientPostgreSQL> {
     async up(info: Info): Promise<void> {
         await this.client.queryArray(
             "CREATE TABLE users (id BIGSERIAL PRIMARY KEY, email TEXT, " +
-                "username VARCHAR(16), hashed_password VARCHAR(64), " +
-                "salt VARCHAR(64), " +
+                "username VARCHAR(16), hashed_password BYTEA(512), " +
+                "client_salt BYTEA(1024), " +
+                "server_salt BYTEA(1024), " +
                 "created_at TIMESTAMP, updated_at TIMESTAMP)"
         );
     }
