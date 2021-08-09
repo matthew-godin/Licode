f1aeb7287090054c0fef8f47b6e262b0ad2ee9b4
matthewgodin <matthew@skillbookacademy.com>
2021-08-09 01:24:44 -0400

    user registration in progress, authentication and authorization on frontend next thing to do

---
+++ react-app/src/components/registerForm.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+export interface RegisterFormProps {}
+
+export interface RegisterFormState {}
+
+class RegisterForm extends React.Component<
+    RegisterFormProps,
+    RegisterFormState
+> {
+    state = {
+        data: { username: "", password: "", name: "" },
+        errors: {},
+    };
+
+    schema = {
+        username: Joi.string().required().email().label("Username"),
+        password: Joi.string().required().min(5).label("Password"),
+        name: Joi.string().required().label("Name"),
+    };
+
+    doSubmit = () => {
+        // Call the server
+        console.log("Submitted");
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Register</h1>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput("username", "Username")}
+                    {this.renderInput("password", "Password", "password")}
+                    {this.renderInput("name", "Name")}
+                    {this.renderButton("Register")}
+                </form>
+            </div>
+        );
+    }
+}
+
+export default RegisterForm;
