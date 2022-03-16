0d1e4eddb03a390c1c59676089f50cc3d3a9aebe
Matthew Godin <m2godin@uwaterloo.ca>
2022-03-16 07:24:18 -0400

    install mui

--- react-app/src/components/codingEditor.tsx
+++ react-app/src/components/codingEditor.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Box, Typography, Grid } from '@mui/material';
 
 export interface FormProps {}
 
@@ -19,9 +20,11 @@ class CodingEditor extends React.Component<FormProps, FormState> {
 
     render() {
         return (
-            <div>
-                sdfsfsdf
-            </div>
+            <Box sx={{ display: 'flex', height: '100vh', bgcolor: 'primary.main', m: 0, p: 0 }}>
+                <Grid container direction="column">
+                    sdfdsf
+                </Grid>
+            </Box>
         );
     }
 }
