ab8a0c5bfb21861e3cf3535bbcd6b271887f494f
Matthew Godin <m2godin@uwaterloo.ca>
2022-03-26 18:35:20 -0400

    user registration with one user as Deno variable

--- react-app/src/components/loginForm.tsx
+++ react-app/src/components/loginForm.tsx
@@ -1,18 +1,43 @@
 import * as React from "react";
 import Form from "./common/form";
 
-export interface LoginFormProps {}
+interface User {
+    email: { value: string };
+    username: { value: string };
+    password: { value: string };
+}
 
-export interface LoginFormState {}
+class LoginForm extends Form {
+    handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        let user: User = {
+            email: { value: '' },
+            username: { value: '' },
+            password: { value: '' },
+        }
+        user.email.value = (e.target as typeof e.target & User).email.value;
+        user.password.value = (e.target as typeof e.target & User).password.value;
+        try {
+            let res = await fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user),
+            })
+                .then(response => response.json())
+                .then(data => console.log(data));
+        } catch (err) {
+            console.log(err);
+        } 
+    };
 
-class SigninForm extends Form {
-    //state = { :  }
     render() {
         return (
             <div>
                 <h1>Login</h1>
-                <form>
-                    {this.renderInput("email_username", "Email or Username")}
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput("email", "Email or Username")}
                     {this.renderInput("password", "Password")}
                     {this.renderButton("Login")}
                 </form>
@@ -21,4 +46,4 @@ class SigninForm extends Form {
     }
 }
 
-export default SigninForm;
+export default LoginForm;
