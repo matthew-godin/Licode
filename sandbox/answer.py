ca9a6efaebe2955921b2913f034a13271c463cd2
Daniel Arevalo <dsareval@uwaterloo.ca>
2022-03-28 20:09:54 -0400

    Push sandbox folder to master

---
+++ sandbox/answer.py
@@ -0,0 +1,21 @@
+def makeSum(nums, target):
+    idxs = sorted(range(len(nums)), key=lambda x: nums[x])
+    start = 0
+    end = len(nums) - 1
+    while True:
+        if (nums[idxs[start]] + nums[idxs[end]]) == target:
+            return (idxs[start], idxs[end])
+        elif (nums[idxs[start]] + nums[idxs[end]]) < target:
+            start += 1
+        else:
+            end -= 1
+
+if __name__ == "__main__":
+    target = int(input())
+    n = int(input())
+    nums = []
+    for i in range(n):
+        nums.append(int(input()))
+    (start, end) = makeSum(nums, target)
+    print(str(start), str(end))
+
