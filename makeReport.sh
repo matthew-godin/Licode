c4ca0b2d90b4c4d174819c31c3ecf8acd0004d73
Matthew Godin <m2godin@uwaterloo.ca>
2022-04-21 16:20:25 -0400

    standard output in editor from sandbox working

---
+++ makeReport.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+reportName=report.txt
+standardOutputName=standardOutput.txt
+outputName=output.txt
+inputExt=in
+outputExt=out
+inputDir=TestInputs
+outputDir=TestOutputs
+
+> "${reportName}"
+> "${standardOutputName}"
+> "${outputName}"
+
+echo "[" >> "${reportName}"
+for input in "${inputDir}"/*."${inputExt}"
+do
+  base=$(basename ${input})
+  pref=${base%.${inputExt}}
+  output="${outputDir}/${pref}.${outputExt}"
+  if diff <(cat "${input}" | python3 answer.py | python3 clean.py) <(cat "${output}" | python3 clean.py) >/dev/null; then
+          echo "{\"testName\": \"${pref}\", \"passed\": true}," >> "${reportName}"
+  else
+          echo "{\"testName\": \"${pref}\", \"passed\": false}," >> "${reportName}"
+  fi
+done
+echo "]" >> "${reportName}"
+
+input=customInput.in
+cat "${input}" | python3 answerCustomInput.py > "${standardOutputName}"
