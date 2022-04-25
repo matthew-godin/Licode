bef00e6e0ed4dd55fcf34393bd8e5f6076a57adc
Matthew Godin <m2godin@uwaterloo.ca>
2022-04-25 15:46:29 -0400

    update questions

---
+++ migrations/2022_04_25_14_39_16_0000004_update_third_question.sql
@@ -0,0 +1,4 @@
+UPDATE questions SET question = 'You are given an integer $integer$. Return 1 if it is a palindrome and 0 otherwise.;A palindrome is a number that can be read forwards or backwards and still be read as the same number.;For instance, 26362 is a palindrome while 263 is not.',
+    function_signature = 'def palindrome(integer):',
+    default_custom_input = '2332'
+    WHERE id = 3;
