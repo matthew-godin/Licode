b30dad1a45db18972ca9d8a60759c9025220323a
Matthew Godin <m2godin@uwaterloo.ca>
2022-06-12 13:00:02 -0400

    fix question 2

--- mod.ts
+++ mod.ts
@@ -195,7 +195,12 @@ function generateStubString(inputFormat: string[], outputFormat: string[], funct
 }
 
 function generateCleanString(outputFormat: string[], normalClean: boolean) {
-    let cleanString = 'import sys\n\nif __name__ == "__main__":\n';
+    let cleanString = '';
+    if (outputFormat[0] != 'aa') {
+        cleanString += 'import sys\n\nif __name__ == "__main__":\n';
+    } else {
+        cleanString += 'import sys\nimport functools\n\ndef compareNns(x, y):\n    if x[0] > y[0]:\n        return 1\n    elif x[0] < y[0]:\n        return -1\n    else:\n        for i in range(x[0]):\n            if x[1][i] > y[1][i]:\n                return 1\n            if x[1][i] < y[1][i]:\n                return -1\n    return 0\n\nif __name__ == "__main__":\n';
+    }
     if (normalClean) {
         cleanString += '    while True:\n        tryInput = input()\n        print("OK", end="", file=sys.stderr)\n        print(tryInput, end="", file=sys.stderr)\n        print("PL", end="", file=sys.stderr)\n        if (tryInput == "v10zg57ZIUF6vjZgSPaDY70TQff8wTHXgodX2otrDMEay0WlS36MjDhHH054uRrFxGHHSegvGcA7eaqB"):\n            break\n';
     }
@@ -205,7 +210,7 @@ function generateCleanString(outputFormat: string[], normalClean: boolean) {
         } else if (outputFormat[0] == 'a') {
             cleanString += '    n = int(input())\n    print("Q", end="", file=sys.stderr)\n    print(n, end="", file=sys.stderr)\n    print("W", end="", file=sys.stderr)\n    nums = []\n    for i in range(n):\n        qw = int(input())\n        print("Q", end="", file=sys.stderr)\n        print(qw, end="", file=sys.stderr)\n        print("W", end="", file=sys.stderr)\n        nums.append(qw)\n    nums.sort()\n    print(n)\n    for i in range(n):\n        print(nums[i])';
         } else if (outputFormat[0] == 'aa') {
-            cleanString += '    n = int(input())\n    print("Q", end="", file=sys.stderr)\n    print(n, end="", file=sys.stderr)\n    print("W", end="", file=sys.stderr)\n    nns = []\n    nums = []\n    for i in range(n):\n        nn = int(input())\n        print("Q", end="", file=sys.stderr)\n        print(nn, end="", file=sys.stderr)\n        print("W", end="", file=sys.stderr)\n        nns.append(nn)\n        nnums = []\n        for j in range(nn):\n            qw = int(input())\n            print("Q", end="", file=sys.stderr)\n            print(qw, end="", file=sys.stderr)\n            print("W", end="", file=sys.stderr)\n            nnums.append(qw)\n        nnums.sort()\n        nums.append(nnums)\n    nums.sort()\n    print(n)\n    for i in range(n):\n        print(nns[i])\n        for j in range(nns[i]):\n            print(nums[i][j])\n';
+            cleanString += '    n = int(input())\n    print("Q", end="", file=sys.stderr)\n    print(n, end="", file=sys.stderr)\n    print("W", end="", file=sys.stderr)\n    nns = []\n    nums = []\n    for i in range(n):\n        nn = int(input())\n        print("Q", end="", file=sys.stderr)\n        print(nn, end="", file=sys.stderr)\n        print("W", end="", file=sys.stderr)\n        nns = nns.copy()\n        nns = []\n        nns.append(nn)\n        nnums = []\n        for j in range(nn):\n            qw = int(input())\n            print("Q", end="", file=sys.stderr)\n            print(qw, end="", file=sys.stderr)\n            print("W", end="", file=sys.stderr)\n            nnums.append(qw)\n        nnums.sort()\n        nns.append(nnums)\n        nums.append(nns)\n    nums.sort(key = functools.cmp_to_key(compareNns))\n    print(n)\n    for i in range(n):\n        print(nums[i][0])\n        for j in range(len(nums[i][1])):\n            print(nums[i][1][j])\n';
         }
     }
     return cleanString;
@@ -804,6 +809,9 @@ router
                     await reportProcess.output();
                     console.log("BBB");
                     let jsonResults: String = await Deno.readTextFile("./sandbox/reportFromPySandbox.txt");
+                    console.log("!!!");
+                    console.log(jsonResults);
+                    console.log("@@@");
                     let standardOutputResults: string = await Deno.readTextFile("./sandbox/standardOutputFromPySandbox.txt");
                     let outputResults: string = await Deno.readTextFile("./sandbox/outputFromPySandbox.txt");
                     let outputResultsSplit: string[] = outputResults.split('\n');
--- sandbox/2/clean.py
+++ sandbox/2/clean.py
@@ -1,4 +1,18 @@
 import sys
+import functools
+
+def compareNns(x, y):
+    if x[0] > y[0]:
+        return 1
+    elif x[0] < y[0]:
+        return -1
+    else:
+        for i in range(x[0]):
+            if x[1][i] > y[1][i]:
+                return 1
+            if x[1][i] < y[1][i]:
+                return -1
+    return 0
 
 if __name__ == "__main__":
     while True:
@@ -19,6 +33,8 @@ if __name__ == "__main__":
         print("Q", end="", file=sys.stderr)
         print(nn, end="", file=sys.stderr)
         print("W", end="", file=sys.stderr)
+        nns = nns.copy()
+        nns = []
         nns.append(nn)
         nnums = []
         for j in range(nn):
@@ -28,10 +44,11 @@ if __name__ == "__main__":
             print("W", end="", file=sys.stderr)
             nnums.append(qw)
         nnums.sort()
-        nums.append(nnums)
-    nums.sort()
+        nns.append(nnums)
+        nums.append(nns)
+    nums.sort(key = functools.cmp_to_key(compareNns))
     print(n)
     for i in range(n):
-        print(nns[i])
-        for j in range(nns[i]):
-            print(nums[i][j])
+        print(nums[i][0])
+        for j in range(len(nums[i][1])):
+            print(nums[i][1][j])
