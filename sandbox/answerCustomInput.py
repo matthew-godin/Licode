8ef6a63db6314d6cb646dedd67b20ad48cce0ee8
Matthew Godin <m2godin@uwaterloo.ca>
2022-06-12 05:54:35 -0400

    fixing questions in progress

--- sandbox/answerCustomInput.py
+++ sandbox/answerCustomInput.py
@@ -1,12 +1,20 @@
-def pairSumIndices(integers, pairSum):
-    i = 0
-    while i < len(integers):
-        j = 0
-        while j < len(integers):
-            if i != j and integers[i] + integers[j] == pairSum:
-                return [i, j]
-            j += 1
-        i += 1
+def arrangementSumPossibleIntegers(integers, arrangementSum):
+    temp = []
+    ans = []
+    arrangementSumPossibleIntegersRecursion(integers, 0, arrangementSum, temp, ans)
+    return ans
+
+def arrangementSumPossibleIntegersRecursion(integers, i, arrangementSum, temp, ans):
+    if arrangementSum == 0:
+        ans.append(temp.copy())
+        return
+    if i == len(integers):
+        return
+    if integers[i] <= arrangementSum:
+        temp.append(integers[i])
+        arrangementSumPossibleIntegersRecursion(integers, i, arrangementSum - integers[i], temp, ans)
+        temp.pop()
+    arrangementSumPossibleIntegersRecursion(integers, i + 1, arrangementSum, temp, ans)
 
 import sys
 
@@ -16,15 +24,14 @@ if __name__ == "__main__":
     print(n0, end="", file=sys.stderr)
     print("H", end="", file=sys.stderr)
     p0 = []
-    nums = []
     for i in range(n0):
         gh = int(input())
         print("G", end="", file=sys.stderr)
         print(gh, end="", file=sys.stderr)
         print("H", end="", file=sys.stderr)
-        nums.append(gh)
+        p0.append(gh)
     p1 = int(input())
     print("G", end="", file=sys.stderr)
     print(p1, end="", file=sys.stderr)
     print("H", end="", file=sys.stderr)
-    result = pairSumIndices(p0, p1)
+    result = arrangementSumPossibleIntegers(p0, p1)
