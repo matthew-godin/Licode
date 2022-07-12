d2981c94a0833ad6fd0a82c51c8208ba340667d7
Matthew Godin <m2godin@uwaterloo.ca>
2022-07-11 22:19:19 -0400

    home page cosmetic improvements

--- mod.ts
+++ mod.ts
@@ -988,7 +988,8 @@ app.use(async (context) => {
     if (!context.request.url.pathname.endsWith('.js')
         && !context.request.url.pathname.endsWith('.png')
         && !context.request.url.pathname.endsWith('.ico')
-        && !context.request.url.pathname.endsWith('.txt'))	{
+        && !context.request.url.pathname.endsWith('.txt')
+        && !context.request.url.pathname.endsWith('.css'))	{
         context.request.url.pathname = '/';
     }
     await context.send({
