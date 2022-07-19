a49884a7d67f60d469a4fd946059197966762f4f
Brayden Riggs <bwriggs@edu.uwaterloo.ca>
2022-07-18 21:53:00 -0400

    Moved validation logic to common and reused it in the backend, added a password tooltip

---
+++ react-app/src/components/common/validation.ts
@@ -0,0 +1,45 @@
+
+export const MIN_PASSWORD_LENGTH = 8;
+export const MAX_PASSWORD_LENGTH = 256;
+export const NUM_PASSWORD_SOFT_REQS = 2;
+
+export function validatePassword(password: string, annoying: boolean) : string {
+    if (!annoying && password.length === 0) {
+        //don't want to annoy the user, they know an empty password is invalid
+        return '';
+    } else if (password.length < MIN_PASSWORD_LENGTH) {
+        return `Password must be at least ${MIN_PASSWORD_LENGTH} characters`
+    } else if (password.length > MAX_PASSWORD_LENGTH) {
+        return `Password must be at most ${MAX_PASSWORD_LENGTH} characters`
+    } else {
+        //must meet NUM_PASSWORD_SOFT_REQS of 4 requirements
+        var hasLower = /[a-z]/.test(password);
+        var hasUpper = /[A-Z]/.test(password);
+        var hasDigit = /\d/.test(password);
+        var hasSpecial = /[^a-zA-Z\d]/.test(password);
+        var softReqsMet = 0;
+        if (hasLower) softReqsMet++
+        if (hasUpper) softReqsMet++
+        if (hasDigit) softReqsMet++
+        if (hasSpecial) softReqsMet++
+        if (softReqsMet < NUM_PASSWORD_SOFT_REQS) {
+            return `Password must have at least ${NUM_PASSWORD_SOFT_REQS} of the following:
+            at least 1 lower case letter
+            at least 1 upper case letter
+            at least 1 number
+            at least 1 special character.`;
+        } else {
+            return ''
+        }
+    }
+}   
+
+export function validateUsername(username: string, annoying: boolean) : string {
+    return '';
+}
+
+export function validateEmail(email: string, annoying: boolean) : string {
+    return '';
+}
+
+
