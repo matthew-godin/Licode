4c7c1bf458863103265c41e6c57e0af69cb46c54
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2023-12-06 08:02:52 -0500

    commit

--- react-app/src/components/waitList.tsx
+++ react-app/src/components/waitList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
 
 const WaitList = () => {
     const navigate = useNavigate();
