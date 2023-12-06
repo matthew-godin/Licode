da9a7849384ef57cfe7f772d83d1c1d62468b7b0
matthew-godin <150533628+matthew-godin@users.noreply.github.com>
2023-12-06 07:47:19 -0500

    Update waitList.tsx

--- react-app/src/components/waitList.tsx
+++ react-app/src/components/waitList.tsx
@@ -7,7 +7,15 @@ const WaitList = () => {
         fetch('/api/matchmaking').then(response => response.json()).then((json) => { navigate('/editor'); });
     }, []);
     return (
-        <div>Waiting to be matched with another player . . . </div>
+        <Box
+                    height="100vh"
+                    sx={{ backgroundColor: '#01182a', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
+                    display="flex" 
+                    flexDirection="column"
+                >
+            <Typography sx={{ fontSize: 56, color: 'white', textAlign: 'center', marginTop: '40vh' }}>
+            Waiting to be matched with another player . . . 
+                    </Typography><Typography sx={{textAlign: 'center', marginTop: '3vh'}}><div className="lds-ring"><div></div><div></div><div></div><div></div></div></Typography></Box>
     );
 }
 
