8037e170d075541a72a7e744a69752f6b5927275
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-02-24 03:55:24 -0500

    delete sid on logout

--- src/rest-api-server/mod.ts
+++ src/rest-api-server/mod.ts
@@ -754,6 +754,7 @@ router
         try {
             let sid = await context.cookies.get('sid');
             if (sid && typeof sid === 'string') {
+                await context.cookies.delete('sid');
                 delete sids[sid as string];
                 context.response.body = { text: 'Successfully Logged Out' };
             }
