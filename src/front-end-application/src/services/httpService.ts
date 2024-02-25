c0d6db2747368059c10e1d70f30ca98d0f6362fe
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-02-24 23:57:04 -0500

    remove unused modules

--- src/front-end-application/src/services/httpService.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    get: 1,
-    post: 1,
-    put: 1,
-    delete: 1,
-};
--- src/front-end-application/src/services/userService.ts
+++
@@ -1,4 +0,0 @@
-import http from "./httpService";
-import { apiUrl } from "../config.json";
-
-const apiEndpoint = apiUrl + "/users";
