49381383156d69748c7875543d5bd8e3a50fd141
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-09 00:37:33 +0000

    Event Controller

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/EventController.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/EventController.java
@@ -1,5 +1,31 @@
 package com.pluralsight.springboot.tickets.events;
 
-public class EventController {
+import java.util.NoSuchElementException;
 
+public class EventController {
+    private final OrganizerRepository organizerRepository;
+    private final EventRepository eventRepository;
+    private final ProductRepository productRepository;
+
+    public EventController(OrganizerRepository organizerRepository, EventRepository eventRepository, ProductRepository productRepository) {
+        this.organizerRepository = organizerRepository;
+        this.eventRepository = eventRepository;
+        this.productRepository = productRepository;
+    }
+
+    public List<Organizer> getOrganizers() {
+        return organizerRepository.findAll();
+    }
+
+    public List<Event> getEventsByOrganizer(int organizerId) {
+        return eventRepository.findByOrganizerId(organizerId);
+    }
+
+    public Event getEventById(int eventId) {
+        return eventRepository.findById(eventId).orElseThrow(() -> throw new NoSuchElementException("Event with id " + eventId + " not found"));
+    }
+
+    public List<Product> getProductsByEvent(int eventId) {
+        return productRepository.findEventById(eventId);
+    }
 }
