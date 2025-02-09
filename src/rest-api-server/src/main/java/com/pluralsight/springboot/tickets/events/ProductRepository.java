636978a08647922527b6c436c5ae17eed17d373a
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-09 02:08:53 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/ProductRepository.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/ProductRepository.java
@@ -3,13 +3,14 @@ package com.pluralsight.springboot.tickets.events;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.math.BigDecimal;
 
 @Repository
 public class ProductRepository {
     private final List<Product> products = List.of(
-        new Product(101, 101, "AAA", "AAAAA", 1.1),
-        new Product(102, 102, "BBB", "BBBBB", 1.2),
-        new Product(103, 103, "CCC", "CCCCC", 1.3)
+        new Product(101, 101, "AAA", "AAAAA", new BigDecimal(1.1)),
+        new Product(102, 102, "BBB", "BBBBB", new BigDecimal(1.2)),
+        new Product(103, 103, "CCC", "CCCCC", new BigDecimal(1.3))
     );
 
     public List<Product> findByEventId(int eventId) {
