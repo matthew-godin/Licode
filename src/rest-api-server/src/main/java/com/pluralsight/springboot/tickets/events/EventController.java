358a2c4faa15ee7c80640edc67dd36e1e0c28dc7
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-09 04:13:01 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/EventController.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/EventController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.NoSuchElementException;
 //import org.springframework.web.bind.annotation.RequestMethod;
