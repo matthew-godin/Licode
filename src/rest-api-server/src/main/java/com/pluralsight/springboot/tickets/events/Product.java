03d33a773f73be0ed16a1b9a9aba00ac76b44934
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-09 06:51:32 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/Product.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/Product.java
@@ -1,13 +1,19 @@
 package com.pluralsight.springboot.tickets.events;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
-public record Product(
-    int id,
-    int eventId,
-    String name,
-    String description,
-    BigDecimal price
-) {
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int eventId;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
 
 }
