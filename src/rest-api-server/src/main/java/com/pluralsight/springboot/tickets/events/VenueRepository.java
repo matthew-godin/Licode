7176f3ec5d3eded30f9b874ec54555108e66e4e3
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-09 07:05:41 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/VenueRepository.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/VenueRepository.java
@@ -1,20 +1,9 @@
 package com.pluralsight.springboot.tickets.events;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class VenueRepository {
-    private final List<Venue> venues = List.of(
-        new Venue(101, "AAA", "AAAAA", "AA", "A"),
-        new Venue(102, "BBB", "BBBBB", "BB", "B"),
-        new Venue(103, "CCC", "CCCCC", "CC", "C"),
-        new Venue(104, "DDD", "DDDDD", "DD", "D")
-    );
-
-    public Optional<Venue> findById(int id) {
-        return venues.stream().filter(venue -> venue.id() == id).findAny();
-    }
+public interface VenueRepository extends JpaRepository<Venue, Integer> {
+    public Optional<Venue> findById(int id);
 }
