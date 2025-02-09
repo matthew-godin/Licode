2fc2397112c1a91e976dd75baea8acf85c8d42c3
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-09 08:34:58 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/registration/Registration.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/registration/Registration.java
@@ -1,14 +1,52 @@
 package com.pluralsight.springboot.tickets.registration;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.persistence.*;
 
-@Document("registrations")
-public record Registration(
-        @Id String id,
-        @NotNull(message = "Product id is required") Integer productId,
-        String ticketCode,
-        @NotBlank(message = "Attendee name is required") String attendeeName) {
+@Entity
+@Table(name = "registrations")
+public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private int productId;
+
+    @Column
+    private String ticketCode;
+
+    @Column
+    private String attendeeName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int id) {
+        this.productId = productId;
+    }
+
+    public int getTicketCode() {
+        return ticketCode;
+    }
+
+    public void setTicketCode(int ticketCode) {
+        this.ticketCode = ticketCode;
+    }
+
+    public int getAttendeeName() {
+        return attendeeName;
+    }
+
+    public void setAttendeeName(int attendeeName) {
+        this.attendeeName = attendeeName;
+    }
 }
