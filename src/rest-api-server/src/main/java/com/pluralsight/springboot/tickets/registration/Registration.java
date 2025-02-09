dde523a2f4442c68b1a456949099abfa5842d450
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-09 04:57:48 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/registration/Registration.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/registration/Registration.java
@@ -1,5 +1,8 @@
 package com.pluralsight.springboot.tickets.registration;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+
 public record Registration(
     int id,
     @NotNull(message = "Product id is required") int productId,
