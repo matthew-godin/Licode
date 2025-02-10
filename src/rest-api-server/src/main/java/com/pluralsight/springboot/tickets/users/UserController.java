6d0d6be0eb3be1a6aa46451a46b9f84ebf675878
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-10 00:38:22 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/users/UserController.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/users/UserController.java
@@ -106,7 +106,7 @@ public class UserController {
                     saltHexString = "0" + saltHexString;
                 }
                 MessageDigest md = MessageDigest.getInstance("SHA-512");
-                md.update(salt.getBytes(StandardCharsets.UTF_8));
+                md.update(saltHexString.getBytes(StandardCharsets.UTF_8));
                 byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
                 StringBuilder sb = new StringBuilder();
                 for(int i = 0; i < bytes.length; ++i){
