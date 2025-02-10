ece384963f71fa0d747239c866fada9c6ab4cf3d
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-10 00:44:46 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/users/UserController.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/users/UserController.java
@@ -116,7 +116,7 @@ public class UserController {
                     for(int i = 0; i < bytes.length; ++i){
                         sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
                     }
-                    String hashedPasswordHexString = sb.toString();
+                    hashedPasswordHexString = sb.toString();
                 } catch (NoSuchAlgorithmException ex) {
                     ex.printStackTrace();
                 }
