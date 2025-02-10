476f45004d542992e05bc7b02a689e74f31ec03c
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-10 02:01:35 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight.springboot.tickets.events;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    public List<Product> findByEventId(int eventId);
-}
--- src/rest-api-server/src/main/java/com/pluralsight/springboot/tickets/events/VenueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight.springboot.tickets.events;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface VenueRepository extends JpaRepository<Venue, Integer> {
-    public Optional<Venue> findById(int id);
-}
