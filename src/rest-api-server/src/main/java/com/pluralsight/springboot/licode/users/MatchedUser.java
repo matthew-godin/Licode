8d4614304f0f41bab24ff9cf6c485b944171baae
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-12 03:24:38 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/MatchedUser.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/MatchedUser.java
@@ -1,3 +1,3 @@
 package com.pluralsight.springboot.licode.users;
 
-public record MatchedUser(String username, int eloRating, String opponentUsername, int opponentEloRating) { }
+public record MatchedUser(String username, Integer eloRating, String opponentUsername, Integer opponentEloRating) { }
