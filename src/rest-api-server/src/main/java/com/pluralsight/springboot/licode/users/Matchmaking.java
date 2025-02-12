318a117083013919f76f07016fe3b4fa3c0e507a
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-12 02:44:09 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/Matchmaking.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/Matchmaking.java
@@ -32,7 +32,7 @@ public class Matchmaking {
         // Partial Fisher-Yates Algorithm for random selection of questions
         for (long i = 0; i < NUM_QUESTIONS_PER_MATCH; ++i) {
             long j = rand.nextLong(numQuestions);
-            Collections.swap(randomPermutation, (int)i, (int)j);
+            Collections.swap(randomPermutation, i.intValue(), j.intValue());
         }
         for (long i = 0; i < NUM_QUESTIONS_PER_MATCH; ++i) {
             questionsSelected.add(randomPermutation.get(i) + 1);
