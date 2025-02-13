165d3a2b5f9bb22d004de110232e116871a21d19
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-13 04:58:32 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/QuestionInformation.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/QuestionInformation.java
@@ -2,4 +2,4 @@ package com.pluralsight.springboot.licode.users;
 
 import java.util.List;
 
-public record QuestionInformation(long questionId, List<String> inputFormat, List<String> outputFormat) { }
+public record QuestionInformation(Long questionId, List<String> inputFormat, List<String> outputFormat) { }
