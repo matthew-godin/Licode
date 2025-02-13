7b8e9ae55d4fd51689e523873798fa9b7d5e5971
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-13 04:40:14 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/TestCasesPassed.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/TestCasesPassed.java
@@ -1,3 +1,6 @@
 package com.pluralsight.springboot.licode.users;
 
+import java.util.List;
+import java.util.Map;
+
 public record TestCasesPassed(List<Boolean> testCasesPassed, String standardOutput, String standardError, String output) { }
