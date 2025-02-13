fb066d5b97d35bdac09c9fce1e4b55820ffd2b76
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-13 05:34:15 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/Win.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/Win.java
@@ -13,8 +13,8 @@ public class Win {
         sidsProgress.remove(opponentSid);
         sidsQuestions.remove(sid);
         sidsQuestions.remove(opponentSid);
-        User user = userRepository.findByUsername(sids.get(sid));
-        User opponent = userRepository.findByUsername(sids.get(opponentSid));
+        User user = userRepository.findByUsername(sids.get(sid)).orElse(null);
+        User opponent = userRepository.findByUsername(sids.get(opponentSid)).orElse(null);
         user.setNumWins(user.getNumWins() + 1);
         double eloRatingVariation = 1 - 1.0 / (1 + Math.pow(10, (opponentEloRating - eloRating) / 400.0));
         user.setEloRating(user.getEloRating() + Math.floor((user.getNumGames() < 30 ? (user.getEloRating() < 2300 ? 40 : 20) : (user.getHas2400RatingHistory() ? 10 : 20)) * eloRatingVariation));
