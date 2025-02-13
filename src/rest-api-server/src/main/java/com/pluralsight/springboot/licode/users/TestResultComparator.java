919e911e3dd7e75d3698984cf5935385394533e5
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-13 05:18:28 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/TestResultComparator.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/TestResultComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class TestResultComparator implements Comparator<TestResult> {
     @Override
     public int compare(TestResult t1, TestResult t2) {
-        return (t1.testName().replaceAll("test", "")) - (t2.testName().replaceAll("test", ""));
+        return +(t1.testName().replaceAll("test", "")) - +(t2.testName().replaceAll("test", ""));
     }
 }
