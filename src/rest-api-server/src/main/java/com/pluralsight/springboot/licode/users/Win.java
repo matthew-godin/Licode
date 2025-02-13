07023da16fbffb34cc2f03e8797402d89643de29
Matthew Godin <matthew.godin.dev@gmail.com>
2025-02-13 05:36:13 +0000

    update

--- src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/Win.java
+++ src/rest-api-server/src/main/java/com/pluralsight/springboot/licode/users/Win.java
@@ -16,10 +16,10 @@ public class Win {
         User user = userRepository.findByUsername(sids.get(sid)).orElse(null);
         User opponent = userRepository.findByUsername(sids.get(opponentSid)).orElse(null);
         user.setNumWins(user.getNumWins() + 1);
-        double eloRatingVariation = 1 - 1.0 / (1 + Math.pow(10, (opponentEloRating - eloRating) / 400.0));
+        double eloRatingVariation = 1 - 1.0 / (1 + Math.pow(10, (opponent.getEloRating() - user.getEloRating()) / 400.0));
         user.setEloRating(user.getEloRating() + Math.floor((user.getNumGames() < 30 ? (user.getEloRating() < 2300 ? 40 : 20) : (user.getHas2400RatingHistory() ? 10 : 20)) * eloRatingVariation));
         opponent.setNumLosses(opponent.getNumLosses() + 1);
-        opponent.setEloRating(opponent.getEloRating() - Math.ceil((opponentNumGames < 30 ? (opponentEloRating < 2300 ? 40 : 20) : (opponentHas2400RatingHistory ? 10 : 20)) * eloRatingVariation));
+        opponent.setEloRating(opponent.getEloRating() - Math.ceil((opponent.getNumGames() < 30 ? (opponent.getEloRating() < 2300 ? 40 : 20) : (opponent.getHas2400RatingHistory() ? 10 : 20)) * eloRatingVariation));
         userRepository.save(user);
         userRepository.save(opponent);
     }
